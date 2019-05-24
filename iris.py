1e71118a2179d681e6cb6f667aa5d4e62bbcf01e
Praneen Shakya <shakyapranin@gmail.com>
2019-05-24 23:27:34 +0545

    Effort to export decision tree visualization as pdf

--- iris.py
+++ iris.py
@@ -7,6 +7,11 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn import tree
 
+# from sklearn.externals.six import StringIO
+# from IPython.display import Image
+# from sklearn.tree import export_graphviz
+# import pydotplus
+
 iris = load_iris()
 
 # print(iris.feature_names)
@@ -37,3 +42,30 @@ classifier = classifier.fit(train_data, train_target)
 
 print(test_target)
 print(classifier.predict(test_data))
+
+#visualize tree
+
+# Ignore the chunk below
+# dot_data = StringIO()
+# export_graphviz(classifier, out_file=dot_data,  
+#                 filled=True, rounded=True,
+#                 special_characters=True)
+# graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+# Image(graph.create_png())
+
+
+from sklearn.externals.six import StringIO
+import pydot
+dot_data = StringIO()
+tree.export_graphviz(classifier,
+    out_file=dot_data,
+    feature_names=iris.feature_names,
+    class_names=iris.target_names,
+    filled=True,
+    rounded=True,
+    impurity=False
+)
+
+graph = pydot.graph_from_dot_data(dot_data.getvalue())
+graph.write_pdf("iris.pdf")
+
